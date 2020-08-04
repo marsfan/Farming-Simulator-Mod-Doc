0d0f870bcec9a14bf46cb0722a75594153463100
Gabe R <Marsfan@users.noreply.github.com>
2020-08-04 14:11:55 -0500

    Add bash script to make schemas from SortedXML

---
+++ utils/genXSD.sh
@@ -0,0 +1,20 @@
+for dir in ./SortedXML/*; do
+    # Get the name of the directory
+    # See https://stackoverflow.com/questions/1371261/get-current-directory-name-without-full-path-in-a-bash-script
+    dirName="${dir%"${dir##*[!/]}"}" # extglob-free multi-trailing-/ trim
+    dirName="${dirName##*/}"
+
+    trang -I xml -O xsd "$dir"/*.xml ../schemas/"$dirName".xsd
+done
+
+mkdir -p ../schemas/vehicle/
+
+
+for dir in SortedXML/vehicle/*; do
+    dirName="${dir%"${dir##*[!/]}"}" # extglob-free multi-trailing-/ trim
+    dirName="${dirName##*/}"
+
+
+    trang -I xml -O xsd "$dir"/*.xml ../schemas/vehicle/"$dirName".xsd
+
+done
