5d96e641393c3c3ae5b79cdb7e92fed355f3eed1
Gabe R <Marsfan@users.noreply.github.com>
2020-08-15 22:46:28 -0500

    Make listing scripts have seperate fcn

--- utils/listPlaceableTypes.py
+++ utils/listPlaceableTypes.py
@@ -2,14 +2,31 @@
 
 import argparse
 from pathlib import Path
-from typing import Set
+from typing import Optional, Set
 
 import re
 
 typeRegex = re.compile(r"<placeableType>(\w*)</placeableType>")
 
 
+def checkPlaceableType(fileData: str) -> Optional[str]:
+    """Check the placeable type for a single file
+
+    Args:
+        fileData: Contents of the file to check
+
+    Returns:
+        The placeable type as a string
+
+    """
+    # Some files have a blank line before data starts. This skips over that if it is there.
+    placeableType = typeRegex.search(fileData)
+    if placeableType is None:
+        return None
+    return placeableType.group(1)
+
 def main():
+    """Run if directly called from command line."""
     parser = argparse.ArgumentParser(description="Sort FS2019 xml files by type")
     parser.add_argument("dataFolder", type=str, help="Data directory to sort")
     args = parser.parse_args()
--- utils/listVehicleTypes.py
+++ utils/listVehicleTypes.py
@@ -6,14 +6,34 @@ Used to add a restriction to the type attribute in vehicle.xsd.
 
 from pathlib import Path
 import argparse
-from typing import Set
+from typing import Optional, Set
 import re
 
-typeRegex = re.compile(r"type=\"([A-z]*)\">")
+typeRegex = re.compile(r"<vehicle.*type=\"([^\"]*)\".*>")
 
+
+def checkVehicleType(fileData: Path) -> Optional[str]:
+    """Check the vehicle type for a single file
+
+    Args:
+        fileData: Contents of the file to check
+
+    Returns:
+        The vehicle type as a string
+
+    """
+
+
+
+    vehicleType: re.Match = typeRegex.search(fileData)
+
+    if vehicleType is None:
+        return None
+    return vehicleType.group(1)
 
 
 def main():
+    """Run if directly called from command line."""
     parser = argparse.ArgumentParser(description="Sort FS2019 xml files by type")
     parser.add_argument("dataFolder", type=str, help="Data directory to sort")
     args = parser.parse_args()
@@ -24,17 +44,8 @@ def main():
     vehicleTypes: Set[str] = set()
 
     for xmlFile in xmlFiles:
-        with open(xmlFile) as fileObj:
-            next(fileObj)
-            fileData = next(fileObj)
-            # Some files have a blank line before data starts. This skips over that if it is there.
-            if fileData == '\n':
-                fileData = next(fileObj)
-            vehicleType: re.Match = typeRegex.search(fileData)
-            if vehicleType is None:
-                vehicleTypes.add("None")
-            else:
-                vehicleTypes.add(vehicleType.group(1))
+        vehicleType = checkVehicleType(xmlFile)
+        vehicleTypes.add(vehicleType.group(1))
 
     [print(vehicleType) for vehicleType in vehicleTypes]
 
